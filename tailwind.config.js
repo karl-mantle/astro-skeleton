d005ba1a2c7a8451902a6eb6a349f950d39f2564
Karl Mantle <130184376+karl-mantle@users.noreply.github.com>
2025-09-29 20:57:09 +0100

    feat: tw styles

---
+++ tailwind.config.js
@@ -0,0 +1,45 @@
+/** @type {import("tailwindcss").Config} */
+module.exports = {
+  theme: {
+    extend: {
+      typography: () => ({
+        primary: {
+          css: {
+            "--tw-prose-body": "var(--color-primary-800)",
+            "--tw-prose-headings": "var(--color-primary-900)",
+            "--tw-prose-lead": "var(--color-primary-700)",
+            "--tw-prose-links": "var(--color-primary-900)",
+            "--tw-prose-bold": "var(--color-primary-900)",
+            "--tw-prose-counters": "var(--color-primary-600)",
+            "--tw-prose-bullets": "var(--color-primary-400)",
+            "--tw-prose-hr": "var(--color-primary-300)",
+            "--tw-prose-quotes": "var(--color-primary-900)",
+            "--tw-prose-quote-borders": "var(--color-primary-300)",
+            "--tw-prose-captions": "var(--color-primary-700)",
+            "--tw-prose-code": "var(--color-primary-900)",
+            "--tw-prose-pre-code": "var(--color-primary-100)",
+            "--tw-prose-pre-bg": "var(--color-primary-900)",
+            "--tw-prose-th-borders": "var(--color-primary-300)",
+            "--tw-prose-td-borders": "var(--color-primary-200)",
+            "--tw-prose-invert-body": "var(--color-primary-200)",
+            "--tw-prose-invert-headings": "var(--color-white)",
+            "--tw-prose-invert-lead": "var(--color-primary-300)",
+            "--tw-prose-invert-links": "var(--color-white)",
+            "--tw-prose-invert-bold": "var(--color-white)",
+            "--tw-prose-invert-counters": "var(--color-primary-400)",
+            "--tw-prose-invert-bullets": "var(--color-primary-600)",
+            "--tw-prose-invert-hr": "var(--color-primary-700)",
+            "--tw-prose-invert-quotes": "var(--color-primary-100)",
+            "--tw-prose-invert-quote-borders": "var(--color-primary-700)",
+            "--tw-prose-invert-captions": "var(--color-primary-400)",
+            "--tw-prose-invert-code": "var(--color-white)",
+            "--tw-prose-invert-pre-code": "var(--color-primary-300)",
+            "--tw-prose-invert-pre-bg": "rgb(0 0 0 / 50%)",
+            "--tw-prose-invert-th-borders": "var(--color-primary-600)",
+            "--tw-prose-invert-td-borders": "var(--color-primary-700)",
+          },
+        },
+      }),
+    },
+  },
+};
